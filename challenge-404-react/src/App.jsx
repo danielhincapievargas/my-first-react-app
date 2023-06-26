61c3609cf69857814d13a5dd4f3a23992a4f5f91
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-06-26 13:45:43 -0500

    challenge 404 en React

---
+++ challenge-404-react/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+        <p className='not-found'>404 NOT FOUND</p>
+        <div className='main-container'>
+        <div className='main-container__img'>
+        </div>
+        <div className='text-container'>
+          <p className='title'>I have bad news for you</p>
+          <p className='parrafo'>the page you are looking for might be removed or is temporarily unavailable</p>
+          <button>BACK TO HOMEPAGE</button>
+        </div>
+        </div>
+      <div className='attribution'>
+        Coded by <a href='#'>Daniel Hincapié Vargas</a>.
+      </div>
+    </div>
+  )
+}
+
+export default App
